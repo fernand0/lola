f43bfb643d81efcbc5dbbae39c0bceadf00f56a3
FernandoTricas García <fernand0@elmundoesimperfecto.com>
2015-05-23 19:34:33 +0200

    Some comments about the configuration data

--- lolaMove.py
+++ lolaMove.py
@@ -6,12 +6,25 @@
 # 
 # Fernando Tricas García
 # https://github.com/fernand0/lola
+#
 
 from serial import Serial
 import time
 import sys
 
+
+# http://www.dfrobot.com/wiki/index.php/Veyron_Servo_Driver_%2824-Channel%29_%28SKU:DRI0029%29
+# We need to send the instructions to the servo controller using the serial
+# port. In this case I'm using it via USB.
+
 ser = Serial('/dev/ttyACM1', 115200)
+
+# Here we are grouping the servos forming legs
+# Each component is a leg:
+# 	. First component is for movement on the plane
+#	. Second and third components are used for moving the leg
+#	  up and down
+
 srvGPIO=[[0,1,2],
          [3,4,5],
          [6,7,8],
@@ -19,21 +32,40 @@ srvGPIO=[[0,1,2],
          [12,13,14],
          [15,16,17]]
 
-srvPOS=[[[2, 0, 90], [2, 2, 135], [2, 1, 90]],  # d-1
-        [[4, 0, 90], [4, 2, 135], [4, 1, 90]],  # d-2
-        [[3, 0, 90], [3, 2, 45],  [3, 1, 90]],  # d-3
-        [[1, 0, 90], [1, 2, 45],  [1, 1, 90]],  # i-1
-        [[5, 0, 90], [5, 2, 45],  [5, 1, 90]],  # i-2
-        [[0, 0, 90], [0, 2, 135], [0, 1, 90]]]  # i-3
+# Here we establish the initial position for each leg. In this position 
+# the hexapod is standing in its 'central' position and it can start moving
+# Notice that the order is a bit 'extrange' because of the way the code
+# has been evolving from the beginning.
+# In any case this way of configuring the thing should allow us to easily
+# reconfigure things. 
+# The first three components are the legs of the right part of the robot
+# (looking from behind) and the other three components correspond to the 
+# legs of the left part of the robot. 
+# The numbers correspond to the position in each part, from 1 to 3 (front,
+# middle, rear).
+
+srvPOS=[[[2, 0, 90], [2, 2, 135], [2, 1, 90]],  # r-1
+        [[4, 0, 90], [4, 2, 135], [4, 1, 90]],  # r-2
+        [[3, 0, 90], [3, 2, 45],  [3, 1, 90]],  # r-3
+        [[1, 0, 90], [1, 2, 45],  [1, 1, 90]],  # l-1
+        [[5, 0, 90], [5, 2, 45],  [5, 1, 90]],  # l-2
+        [[0, 0, 90], [0, 2, 135], [0, 1, 90]]]  # l-3
  
+# Depending on the way the servo was mounted increments (or decrements) are not
+# alwasy consistent across articulations. This matrix helps us to be able to
+# manage the servos in a consitent way.
+
 srvINC=[[-1,-1,+1], 
         [-1,+1,-1], 
         [+1,-1,+1],
         [+1,+1,-1],
         [+1,-1,+1],
         [-1,+1,-1]]
-VEL=100
-WAI=0.1
+
+# Timing for movements and waits between movements
+
+SPEED=100
+WAIT=0.1
 
 
 def angleMap(angle):
@@ -42,9 +74,9 @@ def angleMap(angle):
     return pos
 
 def movePos(art, pos):
-    cadPos="#%d P%d T%d\r"%(art,angleMap(pos),VEL)
+    cadPos="#%d P%d T%d\r"%(art,angleMap(pos),SPEED)
     ser.write(cadPos)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
 def legGPIO(i, serv):
     return srvGPIO[srvPOS[i][serv][0]][srvPOS[i][serv][1]]
@@ -54,7 +86,7 @@ def legPOS(i,serv, inc):
         srvINC[srvPOS[i][serv][0]][srvPOS[i][serv][1]]*inc)
 
 while True:
-    WAI=0.2
+    WAIT=0.2
     print srvPOS
 
     mov1=""
@@ -67,10 +99,10 @@ while True:
         pos=angleMap(legPOS(i+1,2,0))
         mov1=mov1+"#%d P%s "%(legS,pos)
 
-    mov1=mov1+ "T%d\r"%(VEL)
+    mov1=mov1+ "T%d\r"%(SPEED)
     print "Bajar lado 2", mov1
     ser.write(mov1)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
     mov1=""
     for i in [0,2,4]:
@@ -81,10 +113,10 @@ while True:
         legS=legGPIO(i,2)
         pos=angleMap(legPOS(i,2,65))
         mov1=mov1+"#%d P%s "%(legS,pos)
-    mov1=mov1+ "T%d\r"%(VEL)
+    mov1=mov1+ "T%d\r"%(SPEED)
     print "Levantar lado 1", mov1
     ser.write(mov1)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
 
     mov1 = ""
@@ -116,11 +148,11 @@ while True:
             pos=angleMap(legPOS(i,0,-15))
             mov1=mov1+"#%d P%s "%(legS,pos)
 
-    mov1=mov1+ "T%d\r"%(VEL)
+    mov1=mov1+ "T%d\r"%(SPEED)
 
     print "2", mov1
     ser.write(mov1)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
 
     mov1 = ""
@@ -133,10 +165,10 @@ while True:
         pos=angleMap(legPOS(i,2,0))
         mov1=mov1+"#%d P%s "%(legS,pos)
 
-    mov1=mov1+ "T%d\r"%(VEL)
+    mov1=mov1+ "T%d\r"%(SPEED)
     print "Bajar lado 1", mov1
     ser.write(mov1)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
     mov1=""
     for i in [0,2,4]:
@@ -148,10 +180,10 @@ while True:
         pos=angleMap(legPOS(i+1,2,65))
         mov1=mov1+"#%d P%s "%(legS,pos)
 
-    mov1=mov1+ "T%d\r"%(VEL)
+    mov1=mov1+ "T%d\r"%(SPEED)
     print "Levantar lado 2", mov1
     ser.write(mov1)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
     mov1 = ""
     for i in [1,3,5]:
@@ -182,9 +214,9 @@ while True:
             pos=angleMap(legPOS(i,0,-15))
             mov1=mov1+"#%d P%s "%(legS,pos)
 
-    mov1=mov1+ "T%d\r"%(VEL)
+    mov1=mov1+ "T%d\r"%(SPEED)
 
     print "2", mov1
     ser.write(mov1)
-    time.sleep(WAI)
+    time.sleep(WAIT)
 
